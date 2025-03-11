ba13ded0665bf63e129389f7621ea3bcc8385ef6
Natraidor <20220594@ricaldone.edu.sv>
2025-03-11 09:53:38 -0600

    primer commit

---
+++ backend/src/models/Clients.js
@@ -0,0 +1,47 @@
+import { Schema, model } from "mongoose";
+
+const clientsSchema = new Schema(
+{
+    name:{
+        type: String, 
+        
+    },
+
+    lastName:{
+        type: String,
+        require: true,
+    },
+
+    birthday:{
+        type: Date,
+        require: true,
+    },
+
+    email:{
+        type: String,
+        require: true,
+    },
+
+    password:{
+        type: String,
+        require: true,
+    },
+
+    telephone:{
+        type: String,
+        
+    },
+
+    dui:{
+        type: String,
+        require: true,
+    },
+},
+
+    {
+        timestamps: true,
+        strict: false, 
+    }
+);
+
+export default model("Clients", clientsSchema);

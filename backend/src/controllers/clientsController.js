a04826f6205dd0e10a3119fd13f7a2cb0eb77807
Natraidor <20220594@ricaldone.edu.sv>
2025-03-11 10:29:05 -0600

    controlador terminado

--- backend/src/controllers/clientsController.js
+++ backend/src/controllers/clientsController.js
@@ -1,2 +1,33 @@
 
-const clientController = {};
+const clientsController = {};
+
+import clientsModel from "../models/Clients";
+
+clientsController.getClients = async (req, res) => {
+    const clients = await clientsModel.find();
+    res.json(clients);
+  };
+
+  clientsController.insertClients = async (req, res) => {
+    const { name, lastName, birthday, email, password, telephone, dui} = req.body;
+    const newClient = new clientsModel({ name, lastName, birthday, email, password, telephone, dui });
+    await newClient.save();
+    res.json({ message: "Client saved" });
+  };
+
+  clientsController.deleteClients = async (req, res) => {
+    await clientsModel.findByIdAndDelete(req.params.id);
+    res.json({ message: "client deleted" });
+  };
+
+  clientsController.updateClients = async (req, res) => {
+    const {  name, lastName, birthday, email, password, telephone, dui} = req.body;
+    const updateClients = await clientsModel.findByIdAndUpdate(
+      req.params.id,
+      {  name, lastName, birthday, email, password, telephone, dui },
+      { new: true }
+    );
+    res.json({ message: "client updated successfully" });
+  };
+  
+  export default clientsController;
